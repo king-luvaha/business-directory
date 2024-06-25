4ad305252e2dbc00ea450477f70e452b6912a182
Ezra Luvaha <mrluvaha@gmail.com>
2024-06-26 00:32:38 +0300

    Added clerk authentication, setup Home Screen

--- business-directory/app/(tabs)/home.jsx
+++ business-directory/app/(tabs)/home.jsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
 
 export default function home() {
   return (
     <View>
-      <Text style={{fontSize:40, fontFamily:'outfit-bold'}}>home</Text>
+      {/* Header */}
+        <Header />
+      {/* Slider */}
+      {/* Category */}
+      {/* Popular Business List */}
     </View>
   )
 }
